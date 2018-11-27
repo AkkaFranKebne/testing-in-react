8bb0c5da708a47767e73c334050679d70b41f26c
AkkaFranKebne <ewawrobel@gmail.com>
2018-11-27 18:25:57 +0100

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import CommentBox from './CommentBox';
+import CommentList from './CommentList';
+
+export default () => {
+  return (
+    <div>
+      {' '}
+      APP COMP
+      <CommentBox />
+      <CommentList />
+    </div>
+  );
+};
---
+++ src/components/__tests__/App.test.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from '../App';
+
+it('shows the  comment box', () => {});
